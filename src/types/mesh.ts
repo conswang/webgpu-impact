7d54ebbfadf3293074586a73bab7f1d82b5f9318
Eyad <eyadn@seas.upenn.edu>
2022-11-14 21:20:19 -0500

    implemented addFaceIndices(), began working on UVs

--- src/types/mesh.ts
+++ src/types/mesh.ts
@@ -1,14 +1,14 @@
 import { throws } from "assert";
-import { vec3 } from "gl-matrix"
+import { vec3, vec2 } from "gl-matrix"
 
 interface Vertex {
     pos : vec3
-    col : vec3
+    col : vec3 | undefined
+    uv : vec2 | undefined
 }
 
 export class Mesh {
     buffer: GPUBuffer
-    // idxBuffer: GPUBuffer
     bufferLayout: GPUVertexBufferLayout
 
     vertCount: number = 0
@@ -21,20 +21,7 @@ export class Mesh {
 
     constructor(device: GPUDevice) {
         // x y r g b
-        // let verts : Array<Vertex> = new Array<Vertex>();
-        // verts.push({pos: [0.0, -0.5, -0.5], col: [0.0, 1.0, 0.0]});
-        // verts.push({pos: [0.0, 0.0, 0.5], col: [1.0, 0.0, 0.0]});
-        // verts.push({pos: [0.0, 0.5, -0.5], col: [0.0, 0.0, 1.0]});
-        
-        // this.addFace(verts);
         this.createCube();
-
-        // let verts2 : Array<Vertex> = new Array<Vertex>();
-        // verts2.push({pos: [0.0, -0.5, -0.5], col: [0.0, 1.0, 0.0]});
-        // verts2.push({pos: [0.0, 0.0, -2.0], col: [1.0, 0.0, 0.0]});
-        // verts2.push({pos: [0.0, 0.5, -0.5], col: [0.0, 0.0, 1.0]});
-
-        // this.addFace(verts2);
 
         this.populateVBO();
 
@@ -73,7 +60,7 @@ export class Mesh {
         console.log(vert.pos);
         console.log(vert.col);
         this.positions.push(vert.pos);
-        this.colors.push(vert.col);
+        this.colors.push(vert.col!);
         this.vertCount++;
     }
 
@@ -90,6 +77,14 @@ export class Mesh {
         }
     }
 
+    addFaceIndices(indices: Array<number>, col?: vec3) {
+        if (col) {
+            this.faces.push(new Face(indices, col));
+        } else {
+            this.faces.push(new Face(indices));
+        }
+    }
+
     populateVBO() {
         var verts : Array<number> = new Array<number>();
 
@@ -98,41 +93,33 @@ export class Mesh {
 
             // Fan out method per face
             for (let j = 0; j < currFace.vertCount - 2; j++){
+                const col1 : vec3 = (currFace.color != undefined) ? currFace.color! : this.colors[currFace.verts[0]];
+                const col2 : vec3 = (currFace.color != undefined) ? currFace.color! : this.colors[currFace.verts[j + 1]];
+                const col3 : vec3 = (currFace.color != undefined) ? currFace.color! : this.colors[currFace.verts[j + 2]];
+
                 // Vert 0
                 verts.push(this.positions[currFace.verts[0]][0]);
                 verts.push(this.positions[currFace.verts[0]][1]);
                 verts.push(this.positions[currFace.verts[0]][2]);
-                // verts.push(this.colors[currFace.verts[0]][0]);
-                // verts.push(this.colors[currFace.verts[0]][1]);
-                // verts.push(this.colors[currFace.verts[0]][2]);
-
-                verts.push(currFace.color![0]);
-                verts.push(currFace.color![1]);
-                verts.push(currFace.color![2]);
+                verts.push(col1[0]);
+                verts.push(col1[1]);
+                verts.push(col1[2]);
 
                 // Vert j + 1
                 verts.push(this.positions[currFace.verts[j + 1]][0]);
                 verts.push(this.positions[currFace.verts[j + 1]][1]);
                 verts.push(this.positions[currFace.verts[j + 1]][2]);
-                // verts.push(this.colors[currFace.verts[j + 1]][0]);
-                // verts.push(this.colors[currFace.verts[j + 1]][1]);
-                // verts.push(this.colors[currFace.verts[j + 1]][2]);
-                
-                verts.push(currFace.color![0]);
-                verts.push(currFace.color![1]);
-                verts.push(currFace.color![2]);
-                
+                verts.push(col2[0]);
+                verts.push(col2[1]);
+                verts.push(col2[2]);
+                                
                 // Vert j + 2
                 verts.push(this.positions[currFace.verts[j + 2]][0]);
                 verts.push(this.positions[currFace.verts[j + 2]][1]);
                 verts.push(this.positions[currFace.verts[j + 2]][2]);
-                // verts.push(this.colors[currFace.verts[j + 2]][0]);
-                // verts.push(this.colors[currFace.verts[j + 2]][1]);
-                // verts.push(this.colors[currFace.verts[j + 2]][2]);
-
-                verts.push(currFace.color![0]);
-                verts.push(currFace.color![1]);
-                verts.push(currFace.color![2]);
+                verts.push(col3[0]);
+                verts.push(col3[1]);
+                verts.push(col3[2]);
 
                 this.idxCount += 3;
                 console.log("Adding Triangle");
@@ -149,40 +136,40 @@ export class Mesh {
 
     createCube() {
         let f0 : Array<Vertex> = new Array<Vertex>();
-        f0.push({pos: [-1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0]});
-        f0.push({pos: [1.0, -1.0, -1.0], col: [1.0, 0.0, 0.0]});
-        f0.push({pos: [1.0, 1.0, -1.0], col: [0.0, 0.0, 1.0]});
-        f0.push({pos: [-1.0, 1.0, -1.0], col: [0.0, 1.0, 1.0]});
+        f0.push({pos: [-1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0], uv: undefined});
+        f0.push({pos: [1.0, -1.0, -1.0], col: [1.0, 0.0, 0.0], uv: undefined});
+        f0.push({pos: [1.0, 1.0, -1.0], col: [0.0, 0.0, 1.0], uv: undefined});
+        f0.push({pos: [-1.0, 1.0, -1.0], col: [0.0, 1.0, 1.0], uv: undefined});
 
         let f1 : Array<Vertex> = new Array<Vertex>();
-        f1.push({pos: [-1.0, -1.0, 1.0], col: [0.0, 1.0, 0.0]});
-        f1.push({pos: [1.0, -1.0, 1.0], col: [1.0, 0.0, 0.0]});
-        f1.push({pos: [1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0]});
-        f1.push({pos: [-1.0, 1.0, 1.0], col: [0.0, 1.0, 1.0]});
+        f1.push({pos: [-1.0, -1.0, 1.0], col: [0.0, 1.0, 0.0], uv: undefined});
+        f1.push({pos: [1.0, -1.0, 1.0], col: [1.0, 0.0, 0.0], uv: undefined});
+        f1.push({pos: [1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0], uv: undefined});
+        f1.push({pos: [-1.0, 1.0, 1.0], col: [0.0, 1.0, 1.0], uv: undefined});
 
         let f2 : Array<Vertex> = new Array<Vertex>();
-        f2.push({pos: [-1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0]});
-        f2.push({pos: [1.0, -1.0, -1.0], col: [1.0, 0.0, 0.0]});
-        f2.push({pos: [1.0, -1.0, 1.0], col: [0.0, 0.0, 1.0]});
-        f2.push({pos: [-1.0, -1.0, 1.0], col: [0.0, 1.0, 1.0]});
+        f2.push({pos: [-1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0], uv: undefined});
+        f2.push({pos: [1.0, -1.0, -1.0], col: [1.0, 0.0, 0.0], uv: undefined});
+        f2.push({pos: [1.0, -1.0, 1.0], col: [0.0, 0.0, 1.0], uv: undefined});
+        f2.push({pos: [-1.0, -1.0, 1.0], col: [0.0, 1.0, 1.0], uv: undefined});
 
         let f3 : Array<Vertex> = new Array<Vertex>();
-        f3.push({pos: [-1.0, 1.0, -1.0], col: [0.0, 1.0, 0.0]});
-        f3.push({pos: [1.0, 1.0, -1.0], col: [1.0, 0.0, 0.0]});
-        f3.push({pos: [1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0]});
-        f3.push({pos: [-1.0, 1.0, 1.0], col: [0.0, 1.0, 1.0]});
+        f3.push({pos: [-1.0, 1.0, -1.0], col: [0.0, 1.0, 0.0], uv: undefined});
+        f3.push({pos: [1.0, 1.0, -1.0], col: [1.0, 0.0, 0.0], uv: undefined});
+        f3.push({pos: [1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0], uv: undefined});
+        f3.push({pos: [-1.0, 1.0, 1.0], col: [0.0, 1.0, 1.0], uv: undefined});
 
         let f4 : Array<Vertex> = new Array<Vertex>();
-        f4.push({pos: [-1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0]});
-        f4.push({pos: [-1.0, 1.0, -1.0], col: [1.0, 0.0, 0.0]});
-        f4.push({pos: [-1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0]});
-        f4.push({pos: [-1.0, -1.0, 1.0], col: [0.0, 1.0, 1.0]});
+        f4.push({pos: [-1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0], uv: undefined});
+        f4.push({pos: [-1.0, 1.0, -1.0], col: [1.0, 0.0, 0.0], uv: undefined});
+        f4.push({pos: [-1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0], uv: undefined});
+        f4.push({pos: [-1.0, -1.0, 1.0], col: [0.0, 1.0, 1.0], uv: undefined});
 
         let f5 : Array<Vertex> = new Array<Vertex>();
-        f5.push({pos: [1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0]});
-        f5.push({pos: [1.0, 1.0, -1.0], col: [1.0, 0.0, 0.0]});
-        f5.push({pos: [1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0]});
-        f5.push({pos: [1.0, -1.0, 1.0], col: [0.0, 1.0, 1.0]});
+        f5.push({pos: [1.0, -1.0, -1.0], col: [0.0, 1.0, 0.0], uv: undefined});
+        f5.push({pos: [1.0, 1.0, -1.0], col: [1.0, 0.0, 0.0], uv: undefined});
+        f5.push({pos: [1.0, 1.0, 1.0], col: [0.0, 0.0, 1.0], uv: undefined});
+        f5.push({pos: [1.0, -1.0, 1.0], col: [0.0, 1.0, 1.0], uv: undefined});
 
         this.addFace(f0, [1.0, 0.0, 0.0]);
         this.addFace(f1, [0.0, 1.0, 0.0]);
